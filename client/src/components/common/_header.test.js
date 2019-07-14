9d7732b69ec5a4ced09a6423b7312541dbead9fb
john.fasano <john.fasano@bigcommerce.com>
2019-07-14 03:59:59 -0500

    Added initial snopshot testing files for jest

--- client/src/components/common/_header.test.js
+++ client/src/components/common/_header.test.js
@@ -4,7 +4,7 @@ import HeaderBlock from './header.js'
 
 describe("Render HeaderBlock", () => {
   test("Is it rendered and matching", () => {
-    const button = create(<HeaderBlock />)
-    expect(button.toJSON()).toMatchSnapshot()
+    const headBlock = create(<HeaderBlock />)
+    expect(headBlock.toJSON()).toMatchSnapshot()
   })
 })
