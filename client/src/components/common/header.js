9c3f34a2433c27ef20809ed07caa2ea90c916951
john.fasano <john.fasano@bigcommerce.com>
2019-07-14 03:30:59 -0500

    Included the functionality for the content of the page to switch with onClick in Nav

--- client/src/components/common/header.js
+++ client/src/components/common/header.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react';
 
 const navItems = [
   {
-    name: 'Home',
-    link: '#',
-    alt: 'Home'
+    name: "Home",
+    link: "#",
+    alt: "Home",
+    value: "base"
   },
   {
-    name: 'About Me',
-    link: '#about-me',
-    alt: 'About Me'
+    name: "About Me",
+    link: "#about-me",
+    alt: "About Me",
+    value: "about"
   },
   {
-    name: 'Contact Me',
-    link: '#contact-me',
-    alt: 'Contact Me'
+    name: "Contact Me",
+    link: "#contact-me",
+    alt: "Contact Me",
+    value: "contact"
+  },
+  {
+    name: "Projects",
+    link: "#projects",
+    alt: "Projects",
+    value: "projects"
   }
-]
+];
 
-export default class HeaderBlock extends Component {
-  buildNavList() {
-
-  }
+export default class HeaderBlock extends Component { 
   render() {
     return ( 
       <div className="grid">
@@ -30,9 +36,20 @@ export default class HeaderBlock extends Component {
           <ul className="nav">
             {navItems.map((navListItem, index) => {
               return (
-                <li className="menu_item" title={navListItem.name} key={index}>
-                  <a href={navListItem.link} alt={navListItem.alt}>{navListItem.name}</a>
-                </li>)
+                <li
+                  className="menu_item"
+                  title={navListItem.name}
+                  key={index}
+                >
+                  <a
+                    onClick={() => {this.props.setContentBlock(navListItem.value)}}
+                    href={navListItem.link}
+                    alt={navListItem.alt}
+                  >
+                    {navListItem.name}
+                  </a>
+                </li>
+              );
             })}
           </ul>
         </div>
--- client/src/components/index/BasePage.js
+++ client/src/components/index/BasePage.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import HeaderBlock from '../common/header';
 import LogoItem from '../common/logo';
+import Section from '../section/section'
+import About from '../about/about'
 
 export default class BasePage extends Component {
+
+  state = {
+    currentBlock: 'base'
+  }
+
+  setContentBlock = (view) => {
+    this.setState((state) => ({
+      currentBlock: view
+    }))
+  }
+
   render() {
     return (
       <div className="grid">
-        <HeaderBlock />
+        <HeaderBlock setContentBlock={this.setContentBlock}/>
         <div className="main">
           <header className="header row">
             <p>Suh Dude!
@@ -15,6 +28,7 @@ export default class BasePage extends Component {
           </header>
           <section className="content row">
             <LogoItem />
+            <Section block={this.state.currentBlock}/>
           </section>
         </div>
       </div>
