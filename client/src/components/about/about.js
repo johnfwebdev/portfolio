851e443b7d7ca2b21cfef7c6deac0b0b72bfefc6
john.fasano <john.fasano@bigcommerce.com>
2019-07-14 03:30:59 -0500

    Included the functionality for the content of the page to switch with onClick in Nav

---
+++ client/src/components/about/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <article>
+        <p>
+          Working towards improving myself, learning new technologies, and
+          advancing my career.
+        </p>
+      </article>
+    )
+  }
+}
