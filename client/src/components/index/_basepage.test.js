f7625a9dc3efb7072407dcb2b12d8447e7ee2b0c
john.fasano <john.fasano@bigcommerce.com>
2019-07-15 01:10:27 -0500

    added needed install scripts for all driectories and updated readme

--- client/src/components/index/_basepage.test.js
+++ client/src/components/index/_basepage.test.js
@@ -4,7 +4,7 @@ import BasePage from "./BasePage";
 
 describe("Does the full index/base page load? ", () => {
   test("Loaded in comparison to snapshot", () => {
-    const button = create(<BasePage />);
-    expect(button.toJSON()).toMatchSnapshot();
+    const index = create(<BasePage />);
+    expect(index.toJSON()).toMatchSnapshot();
   });
 });
